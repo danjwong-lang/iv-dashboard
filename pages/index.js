760c353ae10e85d8f69d765b9239a1ebd0469b74
danjwong-lang <danjwong@gmail.com>
2025-11-30 09:40:04 -0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://jvamcwzicwhfdjmgatsv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsIn
